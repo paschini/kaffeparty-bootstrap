70d38c0d201e5ee07c2c477ce883d03f755ac3d5
paschini <camila.paschini@gmail.com>
2025-12-05 10:52:19 +0100

    alla produkter

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 // Import our custom CSS
 import '../scss/styles.scss'
+// import { Scrollspy, initMDB } from "mdb-ui-kit";
 
 // Import all of Bootstrap’s JS
 import * as bootstrap from 'bootstrap'
+
+// initMDB({ Scrollspy });
 
 document.getElementById("themeToggle").addEventListener("click", () => {
   const current = document.documentElement.getAttribute("data-theme");
