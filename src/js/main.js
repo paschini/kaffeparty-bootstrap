8ed04f56780ce5708253a2486c68a79d8381a103
paschini <camila.paschini@gmail.com>
2025-12-05 10:52:58 +0100

    tog bort lib som används inte

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 // Import our custom CSS
 import '../scss/styles.scss'
-// import { Scrollspy, initMDB } from "mdb-ui-kit";
 
 // Import all of Bootstrap’s JS
 import * as bootstrap from 'bootstrap'
