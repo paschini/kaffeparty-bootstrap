55418228441aa356059267e96778f89ab25d0a67
paschini <camila.paschini@gmail.com>
2025-12-05 13:14:51 +0100

    användarepreferens och default theme på sidorna

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,22 @@ import * as bootstrap from 'bootstrap'
 
 // initMDB({ Scrollspy });
 
+// Kollar användarepreferens av teman när sidan laddas och bytter till det
+const defaultTheme = document.documentElement.getAttribute("data-bs-theme");
+const userPreference = window.matchMedia("(prefers-color-scheme: dark)") ? "dark" : "light";
+const storedTheme = localStorage.getItem("theme");
+const preferredTheme = storedTheme || userPreference; // sparad teman eller system default
+
+if (defaultTheme === "auto") {
+  document.documentElement.setAttribute("data-bs-theme", preferredTheme);
+} else {
+  document.documentElement.setAttribute("data-bs-theme", defaultTheme);
+}
+
 document.getElementById("themeToggle").addEventListener("click", () => {
-  const current = document.documentElement.getAttribute("data-theme");
+  const current = document.documentElement.getAttribute("data-bs-theme");
   const next = current === "dark" ? "light" : "dark";
-  document.documentElement.setAttribute("data-theme", next);
+
+  document.documentElement.setAttribute("data-bs-theme", next);
+  localStorage.setItem("theme", next);
 });
